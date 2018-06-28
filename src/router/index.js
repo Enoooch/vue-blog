a7efd28788675c7823ebe3c6904d5913aaf51f37
enoch <463777092@qq.com>
2018-06-28 18:13:47 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '*',
+      component: home
+    }
+  ]
+})
