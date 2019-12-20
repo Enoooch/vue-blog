5795e93a3ed4a7d6604158b372614383e534b6cd
Enoooch <tjc.enoch.c@gmail.com>
2019-12-20 10:48:44 +0800

    add 备案

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
-  footer#footer © 2018 Enoooch /
+  footer#footer © 2020 Enoch /
     a(href="mailto:tjc.enoch.c@gmail.com") &nbsp;Contact
+    .placeholder
+    a(href="http://www.beian.miit.gov.cn", target="_blank") 浙ICP备18025059号-1
 </template>
 
 <script>
 export default {
   data() {
-    return {
-
-    }
-  },
-  components: {
-
+    return {}
   }
 }
 </script>
@@ -26,4 +23,6 @@ export default {
     a
       text-decoration: none
       color: inherit
+    .placeholder
+      margin: 20px auto
 </style>
